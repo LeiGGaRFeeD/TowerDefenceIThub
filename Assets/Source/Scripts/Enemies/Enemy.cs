20c06cc551640ec4fb3278788a086ad23c838158
LeiGGaRFeeD <ridefoot@gmail.com>
2023-10-23 19:54:48 +0300

    Added enemies prefabs

---
+++ Assets/Source/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _score;
+    [SerializeField] private int _damage;
+}
---
+++ Assets/Support/ReadME.txt
@@ -0,0 +1,1 @@
+Here will be READ ME important information
