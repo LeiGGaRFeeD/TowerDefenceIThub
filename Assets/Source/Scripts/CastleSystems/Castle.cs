1a555aa63a37a02c89898dd9e06419df974f6da8
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-10-26 15:35:12 +0300

    Something

--- Assets/Source/Scripts/CastleSystems/Castle.cs
+++ Assets/Source/Scripts/CastleSystems/Castle.cs
@@ -9,22 +9,21 @@ public class Castle : MonoBehaviour
     public Slider HealthBar;
     void Start()
     {
-        HealthBar.value = Hp;
+        //HealthBar.value = Hp;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (Hp <= 0)
         {
             Destroy(gameObject);
         }
-        HealthBar.value = Hp;
+        //HealthBar.value = Hp;
     }
 
     public void TakeDamage(int Damage)
     {
         Hp = Hp - Damage;
-        Debug.Log("1'mh3r3");
     }
 }
