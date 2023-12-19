8db528127cb091e2f67461626bea22d23b624dd4
LeiGGaRFeeD <ridefoot@gmail.com>
2023-12-19 15:00:08 +0300

    Tutorial scene created

--- Assets/Source/Scripts/Menus/LevelSelect.cs
+++ Assets/Source/Scripts/Menus/LevelSelect.cs
@@ -21,4 +21,8 @@ public class LevelSelect : MonoBehaviour
     {
         SceneManager.LoadScene("Menu");
     }
+    public void GoToTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
 }
